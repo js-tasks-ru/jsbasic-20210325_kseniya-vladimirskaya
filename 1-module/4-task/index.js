c3111e5dfd707ac56c857e9d8b2007840d536c46
Ksenia Vladimirskaya <Kseniya.Vladimirskaya@mvideo.ru>
2021-04-01 10:35:53 +0300

    убрали строку

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,7 +1,6 @@
 function checkSpam(str) {
   var res = str.toLowerCase();
   if (res.includes('1xbet') || res.includes('xxx')) {
-  //if (('1xBet'.toLowerCase().indexOf('1xBet') === -1 || 'XXX'.toLowerCase().indexOf('XXX') === -1)) {
     return true;
   } else {
     return false;
