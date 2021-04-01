fad79b0992bc2f8f67858d9c15a2d2f2b06c5f94
Ksenia Vladimirskaya <Kseniya.Vladimirskaya@mvideo.ru>
2021-04-01 10:34:18 +0300

    проверка на спам

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,9 @@
 function checkSpam(str) {
-  // ваш код...
+  var res = str.toLowerCase();
+  if (res.includes('1xbet') || res.includes('xxx')) {
+  //if (('1xBet'.toLowerCase().indexOf('1xBet') === -1 || 'XXX'.toLowerCase().indexOf('XXX') === -1)) {
+    return true;
+  } else {
+    return false;
+  }
 }
